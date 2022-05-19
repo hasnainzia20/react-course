93d40bd200acbee49134731e52d5dd8142b1bd75
hasnainzia20 <hasnainzia438@gmail.com>
2022-05-19 14:17:18 +0500

    deleted old components

--- src/App.js
+++ src/App.js
@@ -1,29 +1,7 @@
 import React, { useState } from "react";
-import Expenses from "./expense-tracker-components/Expenses/Expenses";
-
-import NewExpense from "./expense-tracker-components/NewExpenses/NewExpense";
 
 
-const DUMMY_EXPENSES = [
-  {
-    id: 'e1',
-    title: 'Car Insurance',
-    amount: '60.00',
-    date: new Date(2022, 9, 22)
-  },
-  {
-    id: 'e2',
-    title: 'Car',
-    amount: '6.00',
-    date: new Date(2022, 10, 11)
-  },
-  {
-    id: 'e3',
-    title: 'Maintenance',
-    amount: '161.00',
-    date: new Date(2019, 0, 12)
-  }
-]
+
 const App = () => {
   const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
@@ -35,8 +13,7 @@ const App = () => {
 
   return (
     <div>
-      <NewExpense onAddExpense={addExpenseHandler} />
-      <Expenses items={expenses} />
+      <h1>Hello</h1>
     </div>
   );
 }
