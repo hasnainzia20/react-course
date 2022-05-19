1aa21d9167883fcdd3567a582c652f0a44ae6e0d
hasnainzia20 <hasnainzia438@gmail.com>
2022-05-19 14:19:26 +0500

    starting form start

--- src/App.js
+++ src/App.js
@@ -1,16 +1,6 @@
 import React, { useState } from "react";
 
-
-
 const App = () => {
-  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
-
-  const addExpenseHandler = expense => {
-    setExpenses(prevExpenses => {
-      return [expense, ...prevExpenses]
-    });
-  }
-
   return (
     <div>
       <h1>Hello</h1>
