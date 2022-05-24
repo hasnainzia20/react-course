1d31cdc01af8e11054bce06c2d9bca0073c6f499
@ <hasnainzia438@gmail.com>
2022-05-24 11:24:46 +0500

    added image

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
+import mealsImage from '../../assets/meals.jpg';
+
 const Header = props => {
     return (
         <>
-
+            <header>
+                <h1>Meals</h1>
+                <button>Cart</button>
+            </header>
+            <div>
+                <img src={mealsImage} />
+            </div>
         </>
     )
 }
