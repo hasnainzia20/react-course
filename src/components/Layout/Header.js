bf94a5cb91041d7c4340bd2b6c0872b09baaf7a1
@ <hasnainzia438@gmail.com>
2022-05-24 12:01:32 +0500

    added header section

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,16 +1,18 @@
 import React from 'react';
+import HeaderCartButton from './HeaderCartButton';
 
 import mealsImage from '../../assets/meals.jpg';
+import classes from './Header.module.css';
 
 const Header = props => {
     return (
         <>
-            <header>
+            <header className={classes.header}>
                 <h1>Meals</h1>
-                <button>Cart</button>
+                <HeaderCartButton />
             </header>
-            <div>
-                <img src={mealsImage} />
+            <div className={classes['main-image']} >
+                <img src={mealsImage} alt='Delicious Food' />
             </div>
         </>
     )
